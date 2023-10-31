aa4f1ab593e9136ba3121b4c8f4c2e4e67018e5d
Qi Wang <qi@broadinstitute.org>
2023-10-31 11:07:38 -0400

    update publish script

--- terra-app-helm/publish.sh
+++ terra-app-helm/publish.sh
@@ -4,10 +4,11 @@
 set -eu
 
 CHART_NAME=$1
+VERSION=$2
 
 cd terra-app-helm
 helm package $CHART_NAME
 mkdir -p $CHART_NAME/repo
-mv $CHART_NAME-0.1.0.tgz $CHART_NAME/repo/
+mv $CHART_NAME-$VERSION.tgz $CHART_NAME/repo/
 helm repo index $CHART_NAME/repo/ --url https://storage.googleapis.com/terra-app-helm/$CHART_NAME
 gsutil cp -r $CHART_NAME/repo/* gs://terra-app-helm/$CHART_NAME
